a654a955d678e59d8f75375e5ee80ebfe792d77a
Hendi Santika <hendisantika@yahoo.co.id>
2021-12-27 09:46:38 +0700

    #2 - Add required libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.keycloak:keycloak-spring-boot-starter:16.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
