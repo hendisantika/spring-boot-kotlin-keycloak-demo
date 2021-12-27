a7b5df30bbd9cd369acf54243775c8ccd9cd8066
Hendi Santika <hendisantika@yahoo.co.id>
2021-12-27 09:58:11 +0700

    #9 - Add KeycloakController

---
+++ src/main/kotlin/com/hendisantika/controller/KeycloakController.kt
@@ -0,0 +1,19 @@
+package com.hendisantika.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-keycloak-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/21
+ * Time: 09.57
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+@RequestMapping("/api")
+class KeycloakController
