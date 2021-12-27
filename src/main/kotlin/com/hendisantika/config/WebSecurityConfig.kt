fdaf2df5b351601cf45c9f29feb9e0e36ab997cc
Hendi Santika <hendisantika@yahoo.co.id>
2021-12-27 09:55:17 +0700

    #6 - Add sessionAuthenticationStrategy in WebSecurityConfig

--- src/main/kotlin/com/hendisantika/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/hendisantika/config/WebSecurityConfig.kt
@@ -3,9 +3,12 @@ package com.hendisantika.config
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,10 @@ class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
         val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
         keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
         auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+
+    @Bean
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return NullAuthenticatedSessionStrategy()
     }
 }
